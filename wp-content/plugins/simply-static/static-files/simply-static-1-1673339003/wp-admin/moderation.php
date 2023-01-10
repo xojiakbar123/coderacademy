8149a7cecfbd3481bc9dcb4ca2d263cdb9eebecd
xojiakbar123 <102510848+xojiakbar123@users.noreply.github.com>
2023-01-10 03:24:30 -0500

    New Website

---
+++ wp-content/plugins/simply-static/static-files/simply-static-1-1673339003/wp-admin/moderation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=/wp-admin/edit-comments.php?comment_status=moderated">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/wp-admin/edit-comments.php?comment_status=moderated";
+		</script>
+
+		<p>You are being redirected to <a href="/wp-admin/edit-comments.php?comment_status=moderated">/wp-admin/edit-comments.php?comment_status=moderated</a></p>
+	</body>
+</html>
